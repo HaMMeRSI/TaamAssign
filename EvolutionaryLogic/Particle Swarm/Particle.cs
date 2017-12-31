ee0dcb59b610a52ae3af174cfad7155bee1b1af9
HuMMeR <sagih95@gmail.com>
2017-12-31 19:38:31 +0200

    Start of swarm

---
+++ EvolutionaryLogic/Particle Swarm/Particle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizationLogics
+{
+    public class Particle
+    {
+        public IDNA DNA { get; set; }
+        public IDNA PBest { get; set; }
+        public int[] Velocity { get; set; }
+        public float Fitness
+        {
+            get
+            {
+                return this.DNA.GetFitnesss();
+            }
+        }
+
+        public Particle(Func<IDNA> GetDNA)
+        {
+            this.DNA = GetDNA();
+            //this.Velocity = new int[this.DNA]
+        }
+
+        public void Execute()
+        {
+            this.DNA.Execute();
+        }
+
+        public void CalculateFitness()
+        {
+            this.DNA.CalculateFitness();
+        }
+    }
+}
---
+++ EvolutionaryLogic/Particle Swarm/ParticleSwarm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizationLogics
+{
+    public class ParticleSwarm
+    {
+        public Particle BestParticle { get; set; }
+        public Particle[] Particles { get; set; }
+        public CStatutsGraph StatusGraph { get; set; }
+
+        public ParticleSwarm(Func<IDNA> GetDNA)
+        {
+            this.StatusGraph = new CStatutsGraph();
+
+            this.Particles[0] = new Particle(GetDNA);
+            this.BestParticle = this.Particles[0];
+            this.BestParticle.Execute();
+            this.BestParticle.CalculateFitness();
+
+
+            for (int i = 1; i < this.Particles.Length; i++)
+            {
+                this.Particles[i] = new Particle(GetDNA);
+                this.Particles[i].Execute();
+                this.Particles[i].CalculateFitness();
+
+                if(this.Particles[i].Fitness > this.BestParticle.Fitness)
+                {
+                    this.BestParticle = this.Particles[i];
+                }
+            }
+        }
+
+        public void Search()
+        {
+            for (int i = 0; i < this.Particles.Length; i++)
+            {
+                // this.Particles[i].DNA = ;
+                // this.Particles[i].Velocity = ;
+            }
+        }
+    }
+}
